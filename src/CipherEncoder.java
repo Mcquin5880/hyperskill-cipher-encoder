4db02ee9aaf870b927d8754d76087b850a58df32
Michael <mquinter88@gmail.com>
2025-01-03 01:18:42 -0500

    stage 3/5 complete!

--- src/CipherEncoder.java
+++ src/CipherEncoder.java
@@ -10,11 +10,48 @@ public class CipherEncoder {
             String input = sc.nextLine();
             System.out.println("\nThe result:");
 
+            StringBuilder binaryString = new StringBuilder();
             for (char c : input.toCharArray()) {
                 String binary = Integer.toBinaryString(c);
                 String paddedBinary = String.format("%7s", binary).replace(' ', '0');
-                System.out.printf("%c = %s%n", c, paddedBinary);
+                binaryString.append(paddedBinary);
+            }
+
+            String encoded = encodeBinary(binaryString.toString());
+
+            System.out.println(encoded);
+        }
+    }
+
+    private static String encodeBinary(String binary) {
+
+        StringBuilder sb = new StringBuilder();
+        char[] chars = binary.toCharArray();
+
+        char currentBit = chars[0];
+        int count = 1;
+
+        for (int i = 1; i < chars.length; i++) {
+            if (chars[i] == currentBit) {
+                count++;
+            } else {
+                appendEncodedSegment(sb, currentBit, count);
+                sb.append(" ");
+                currentBit = chars[i];
+                count = 1;
             }
         }
+
+        appendEncodedSegment(sb, currentBit, count);
+        return sb.toString();
+    }
+
+    private static void appendEncodedSegment(StringBuilder sb, char bit, int length) {
+        if (bit == '1') {
+            sb.append("0 ");
+        } else {
+            sb.append("00 ");
+        }
+        sb.append("0".repeat(length));
     }
 }
